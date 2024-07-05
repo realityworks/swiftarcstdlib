4554b8ee70bece34d16e90951415121b33e8180a
Piotr Suwara <psuwara@realityworks.com.au>
2024-07-05 12:03:42 +1000

    [INTERNAL] Fixed path

--- Package.swift
+++ Package.swift
@@ -19,7 +19,9 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "swiftarcstdlib"),
+            name: "swiftarcstdlib",
+            path: "Sources/"
+        ),
         .testTarget(
             name: "swiftarcstdlibTests",
             dependencies: ["swiftarcstdlib"]),
