34156df306f6e107395871d43f75257125133ceb
Piotr Suwara <psuwara@realityworks.com.au>
2024-07-25 09:27:12 +1000

    [FIX] Removed complexity around BCD extension

--- Sources/Types/BCD.swift
+++ Sources/Types/BCD.swift
@@ -7,32 +7,18 @@
 
 import Foundation
 
-public protocol BCDConvertible {
-    associatedtype ValueType: BinaryInteger
-    var bcd: ValueType { get }
-}
-
-public extension BCDConvertible where Self: BinaryInteger {
-    var bcd: ValueType {
-        var value: ValueType = 0
+public extension BinaryInteger {
+    var bcd: Self {
+        var value: Self = 0
         var numeral: Int = 0
         var digits = self
         while digits != 0 {
             let digit = digits % 10
-            value = value | ValueType(digit) << (4 * numeral)
+            value = value | Self(digit) << (4 * numeral)
             numeral += 1
             digits = (digits / 10)
         }
         return value
     }
 }
-
-extension UInt8: BCDConvertible {
-    public typealias ValueType = Self
-}
-
-extension UInt16: BCDConvertible {
-    public typealias ValueType = Self
-}
-
 
