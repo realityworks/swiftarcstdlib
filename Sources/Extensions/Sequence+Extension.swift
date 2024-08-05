ed48ec3803a2fd1807b69052fb711b2e96878869
Piotr Suwara <psuwara@realityworks.com.au>
2024-08-05 10:29:28 +1000

    [INTERNAL] Added anySatisfy function to make it easier to check value in a sequence

--- Sources/Extensions/Sequence+Extension.swift
+++ Sources/Extensions/Sequence+Extension.swift
@@ -5,8 +5,28 @@
 import Foundation
 
 public extension Sequence where Element: Hashable {
+
+    ///
+    /// Provide a subset of a sequence that contains only unique elements in an array.
+    /// - Parameters:
+    ///   - predicate: A predicate taking an element type returning a boolean if it is satisified.
+    /// - Returns: An array of all unique elements of the sequence.
     func uniqued() -> [Element] {
         var set = Set<Element>()
         return filter { set.insert($0).inserted }
+    }
+
+    ///
+    /// Check if any element in the sequence satisfies the predicate
+    /// - Parameters:
+    ///   - predicate: A predicate taking an element type returning a boolean if it is satisified.
+    /// - Returns: A boolean indicating if there is at least one element satisfying the predicate.
+    func anySatisfy(_ predicate: (Element)->Bool) -> Bool {
+        for element in self {
+            if predicate(element) {
+                return true
+            }
+        }
+        return false
     }
 }
